3e1cd9c7b4bdfa2d42e34233b608f960733e36ad
kawaki-san <dev@kanjala.com>
2023-04-06 23:40:14 +0200

    start web server

--- crates/sellershut/src/main.rs
+++ crates/sellershut/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(handler));
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {address}");
+
+    if let Err(e) = axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+    {
+        eprintln!("{e}")
+    }
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, Rust</h1>")
 }
