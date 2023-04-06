13529b648ff9303d296805ad39c93ed72a6d5772
kawaki-san <dev@kanjala.com>
2023-04-07 01:45:54 +0200

    graphql: expose playground

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,16 +1,36 @@
 mod log;
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing, Router};
+use api::{build_schema, AppSchema};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    response::{Html, IntoResponse},
+    routing, Extension, Router,
+};
+use entity::async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use tower_http::trace::TraceLayer;
 use tracing::{error, info};
 
+#[cfg(debug_assertions)]
+use dotenvy::dotenv;
+
+const ENDPOINT_PATH: &str = "/api/graphql";
+
 #[tokio::main]
 async fn main() {
+    #[cfg(debug_assertions)]
+    dotenv().ok();
+
     log::initialise();
 
+    let schema = build_schema().await;
+
     let app = Router::new()
-        .route("/", routing::get(handler))
+        .route(
+            ENDPOINT_PATH,
+            routing::get(graphql_playground).post(handler),
+        )
+        .layer(Extension(schema))
         .layer(TraceLayer::new_for_http());
 
     let address = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -24,6 +44,12 @@ async fn main() {
     }
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, Rust</h1>")
+async fn handler(schema: Extension<AppSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new(
+        ENDPOINT_PATH,
+    )))
 }
